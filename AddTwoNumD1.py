5712d6b148b86dd85ad3bbc56821ba94f18bff86
ABHISHEK CHAUDHARY <153168708+DashingData@users.noreply.github.com>
2024-12-27 16:48:08 +0530

    Adding two numbers
    This  is a very simple and basic code for printing and adding to number in python

---
+++ AddTwoNumD1.py
@@ -0,0 +1,14 @@
+# Adding two numbers (with pre-defimed varibales)
+num1 = 20
+num2 = 34
+result = num1 + num2
+print("The sum fo the two numbers is :" ,  result)
+
+
+
+
+# methord : 2  (accepting the values of the two numbers)
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+result = num1 + num2
+print("The sum of the two numbers is:" , result) 
