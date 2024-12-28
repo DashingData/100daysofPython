af166d71ab151d020b187565b00f3303e8c01efe
ABHISHEK CHAUDHARY <153168708+DashingData@users.noreply.github.com>
2024-12-28 10:45:46 +0530

    Finding the Square root of a number
    In this code i made a program to find the square root of the number

---
+++ FindingTheSqrD3.py
@@ -0,0 +1,19 @@
+# M:2 using the exponential 
+
+num1 = 64 
+sr = num1**0.5
+print("the exponential of the number is:", sr)
+
+# M: 2 usnig the user defined number 
+
+num2 = int(input("Enter the number:"))
+sr = num2**0.5
+print("The exponetial of the number is:" , sr)
+
+
+
+# M : 3 using the maths module 
+import math
+num = int(input("Enter the number:"))
+sr = math.sqrt(num)
+print(sr)
