a069ef91bd58bb552b97f824b957a8098da363f3
ABHISHEK CHAUDHARY <153168708+DashingData@users.noreply.github.com>
2025-01-01 23:31:16 +0530

    Random Number
    In this program i made a code to get the random number

---
+++ D7ProgramforRandomNo.py
@@ -0,0 +1,7 @@
+# Program to generate the random numbers
+
+import random
+num = random.randint(0 , 2) # randint finds a random number from the given numbers
+num2 = random.randrange(30 , 40,  2 ) # in randrange we can become specify as we can choose the differemce between the mumbers also randrange excludes the last number like 40 is not included here 
+print(num)
+print(num2)
