82cc9d306d74bdfb923cba2020a7628c86bce491
ABHISHEK CHAUDHARY <153168708+DashingData@users.noreply.github.com>
2025-01-02 23:48:13 +0530

    Convert KM to Miles
    In this Program i did a program to find convert KM in to miles and vice versa

---
+++ D8ConvertKmTomiles.py
@@ -0,0 +1,10 @@
+# Python program to convert km into miles
+# 1 Km = 0.621371 miles
+
+kilometre = int(input("Enter the value in kilometres:"))
+
+miles = (0.621371)*kilometre
+
+print(kilometre , "Km in miles will be" , miles , "miles")          
+
+        
