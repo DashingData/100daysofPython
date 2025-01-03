7781abe31feea0408f8f531ee1cf8c0ae6e6e4c3
ABHISHEK CHAUDHARY <153168708+DashingData@users.noreply.github.com>
2025-01-03 13:47:04 +0530

    Temperature conversion
    In this code i coded to convert celcius into feherenite and vice versa

---
+++ D9ConvertCelciusToFeh.py
@@ -0,0 +1,8 @@
+#Converting the celcius to fehrenite 
+# 0 celcius = 32 fahrenheit
+
+celcius = int(input("Enter the temperature in the celcius:"))
+fahrenheit = (celcius*(9/5))+32
+
+print("The converted value is:", fahrenheit )
+
